d7d7385e414d922be547831b7899176ca2cdf6fe
NativeStar <z2858470935@163.com>
2026-02-09 19:18:54 +0800

    电池状态更新时上报doze

--- app/src/main/java/com/suisho/linktocomputer/receiver/BatteryStateReceiver.java
+++ app/src/main/java/com/suisho/linktocomputer/receiver/BatteryStateReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.BatteryManager;
+import android.os.PowerManager;
 
 import com.suisho.linktocomputer.responseBuilders.DeviceStateUpdatePacket;
 import com.suisho.linktocomputer.service.ConnectMainService;
@@ -14,15 +15,17 @@ import org.slf4j.LoggerFactory;
 public class BatteryStateReceiver extends BroadcastReceiver {
     private final ConnectMainService networkService;
     private final Logger logger = LoggerFactory.getLogger(BatteryStateReceiver.class);
+    private final PowerManager powerManager;
     public BatteryStateReceiver(ConnectMainService service) {
         this.networkService=service;
+        powerManager= (PowerManager) service.getSystemService(Context.POWER_SERVICE);
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if(networkService.isConnected){
             logger.debug("Send battery state update packet");
-            DeviceStateUpdatePacket updatePacket=new DeviceStateUpdatePacket(networkService,intent);
+            DeviceStateUpdatePacket updatePacket=new DeviceStateUpdatePacket(networkService,intent,powerManager);
             updatePacket.setBatteryLevel(intent.getIntExtra(BatteryManager.EXTRA_LEVEL,-1));
             updatePacket.setBatteryTemp(intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE,-1));
             networkService.sendString(updatePacket.build().toString());
--- app/src/main/java/com/suisho/linktocomputer/responseBuilders/DeviceStateUpdatePacket.java
+++ app/src/main/java/com/suisho/linktocomputer/responseBuilders/DeviceStateUpdatePacket.java
@@ -4,6 +4,7 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.BatteryManager;
+import android.os.PowerManager;
 
 import com.google.gson.JsonObject;
 
@@ -15,14 +16,16 @@ public class DeviceStateUpdatePacket {
     private int batteryLevel=-1;
     private int batteryTemp=-1;
     private final Intent broadcastIntent;
+    private final PowerManager powerManager;
     private final Logger logger = LoggerFactory.getLogger(DeviceStateUpdatePacket.class);
 
 
-    public DeviceStateUpdatePacket(Context context,Intent intent) {
-        this.appContext = context;
-        this.broadcastIntent=intent;
-    }
 
+    public DeviceStateUpdatePacket(Context context, Intent intent, PowerManager powerManager) {
+        this.appContext = context;
+        this.broadcastIntent = intent;
+        this.powerManager = powerManager;
+    }
     public void setBatteryLevel(int level) {
         this.batteryLevel = level;
     }
@@ -42,6 +45,8 @@ public class DeviceStateUpdatePacket {
         jsonObject.addProperty("batteryTemp",this.batteryTemp==-1?null:this.batteryTemp);
         //是否充电
         jsonObject.addProperty("charging", isCharging());
+        //doze模式
+        jsonObject.addProperty("inDoze", powerManager.isDeviceIdleMode());
         logger.debug("Created device state update packet");
         return jsonObject;
     }
