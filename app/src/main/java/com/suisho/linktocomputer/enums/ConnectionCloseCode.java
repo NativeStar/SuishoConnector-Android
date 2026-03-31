c92564152fe3cf18951435c1a7297968c9663c70
NativeStar <z2858470935@163.com>
2026-03-31 19:41:53 +0800

    更新版本号

--- app/src/main/java/com/suisho/linktocomputer/enums/ConnectionCloseCode.java
+++ app/src/main/java/com/suisho/linktocomputer/enums/ConnectionCloseCode.java
@@ -7,8 +7,6 @@ public class ConnectionCloseCode {
     public final static int CloseFromServer=1001;
     //手机端异常关闭
     public final static int CloseFromClientError=1002;
-    //手机端wifi断开
-    public final static int CloseFromClientWifiDisconnect=1003;
     //手机端崩溃
     public final static int CloseFromClientCrash=1007;
 }
